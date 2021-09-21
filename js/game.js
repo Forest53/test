5ccfb65a23ee658d3a77423851a1bde72a8db10e
Forest53 <71834234+Forest53@users.noreply.github.com>
2021-09-21 21:02:45 +0600

    Test
    Test

---
+++ js/game.js
@@ -0,0 +1,109 @@
+var cvs = document.getElementById("canvas");
+var ctx = cvs.getContext("2d");
+
+var bird = new Image();
+var bg = new Image();
+var fg = new Image();
+var pipeUp = new Image();
+var pipeBottom = new Image();
+
+bird.src="img/flappy_bird_bird.png";
+bg.src="img/flappy_bird_bg.png";
+fg.src="img/flappy_bird_fg.png";
+pipeUp.src="img/flappy_bird_pipeUp.png";
+pipeBottom.src="img/flappy_bird_pipeBottom.png";
+
+
+//zvuc
+var score_audio = new Audio();
+
+score_audio.src = "audio/score.mp3"
+
+
+
+gap = 120;
+
+
+
+
+
+//deistvie
+document.addEventListener("keydown",moveUp);
+
+function moveUp(){
+	yPos -=35;
+}
+
+
+//block
+var pipe =[];
+
+pipe[0]={
+	x:cvs.width, 
+	y:0
+}
+
+
+var score = 0;
+//position bird
+var xPos = 10;
+var yPos = 150;
+var gray = 1.5 ;
+
+
+function draw(){
+	ctx.drawImage(bg,0,0);
+
+
+for(var i = 0;i<pipe.length;i++){
+  ctx.drawImage(pipeUp,pipe[i].x,pipe[i].y);
+  ctx.drawImage(pipeBottom,pipe[i].x, pipe[i].y + pipeUp.height + gap);
+
+  pipe[i].x--;
+
+  if(pipe[i].x ==90){
+  	pipe.push({
+  		x:cvs.width,
+  		y:Math.floor(Math.random()*pipeUp.height)-
+  		pipeUp.height
+  	});
+  }
+
+  if(xPos + bird.width>=pipe[i].x
+  	&& xPos <= pipe[i].x + pipeUp.width
+  	&&(yPos <=pipe[i].y + pipeUp.height
+  		|| yPos + bird.height >=pipe[i].y + pipeUp.height+
+  		gap)|| yPos + bird.height >=cvs.height - fg.height){
+  	location.reload();//perezapusk
+  }
+
+  if (pipe[i].x == 5){
+  	score++;
+  	score_audio.play();
+  }
+
+
+}
+  ctx.drawImage(fg,0,cvs.height - fg.height);
+  ctx.drawImage(bird,xPos,yPos);
+
+  yPos += gray;
+
+  ctx.fillStyle = "#000";
+  ctx.font = "24px Verdana";
+  ctx.fillText("Счет:" + score,10, cvs.height - 20)
+
+  requestAnimationFrame(draw);
+}
+
+pipeBottom.onload = draw; 
+
+
+
+
+
+
+
+
+
+
